1580094c47f269b384bcbf6a18dc452be37efe6c
Ignacio Ramos Sánchez <nachoramossanchez@gmail.com>
2023-05-19 16:10:38 +0200

    Update HomeScreen.kt

--- features/home/src/main/kotlin/com/wachon/spotiwrap/features/home/presentation/HomeScreen.kt
+++ features/home/src/main/kotlin/com/wachon/spotiwrap/features/home/presentation/HomeScreen.kt
@@ -14,11 +14,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -144,18 +146,23 @@ fun HomeTopGenres(
         BoxWithConstraints(
             Modifier
                 .padding(horizontal = 16.dp, vertical = 8.dp)
+                .clip(RoundedCornerShape(30))
+                .height(28.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
         ) {
             Box(
                 modifier = Modifier
+                    .padding(6.dp)
                     .animateContentSize()
-                    .clip(RoundedCornerShape(8.dp))
-                    .height(24.dp)
+                    .clip(RoundedCornerShape(30))
+                    .height(28.dp)
                     .fillMaxWidth(fraction = width)
                     .background(color = BubblegumPink)
             )
             TextNoPadding(
                 modifier = Modifier
-                    .padding(horizontal = 8.dp)
+                    .padding(horizontal = 12.dp)
                     .align(Alignment.CenterStart),
                 text = genre.genreName,
                 style = SubBody.copy(fontWeight = FontWeight.W700)
