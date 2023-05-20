87e2704bdbf903d376ea8ee42cbc357c84677593
Nacho Ramos Sanchez <nachoramossanchez@gmail.com>
2023-05-20 10:42:41 +0200

    Refactor use case

--- features/home/src/main/kotlin/com/wachon/spotiwrap/features/home/domain/GetTopGenres.kt
+++ features/home/src/main/kotlin/com/wachon/spotiwrap/features/home/domain/GetTopGenres.kt
@@ -1,6 +1,7 @@
 package com.wachon.spotiwrap.features.home.domain
 
 import com.wachon.spotiwrap.core.common.dispatchers.DispatcherProvider
+import com.wachon.spotiwrap.core.common.extensions.capitalizeWords
 import com.wachon.spotiwrap.core.common.model.ArtistModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -19,28 +20,22 @@ class GetTopGenresFromArtists(
     override fun invoke(artistsFlow: Flow<List<ArtistModel>>): Flow<List<TopGenreUI>> {
         return artistsFlow.flatMapLatest { artists ->
             flow {
-                val counts = artists.flatMap { it.genres }
+                val counts = artists
+                    .flatMap { it.genres }
                     .filterNotNull()
                     .filter { it.isNotBlank() }
                     .groupingBy { it }
                     .eachCount()
                     .toList()
                     .sortedByDescending { it.second }
-                    .map {
-                        it.first
-                            .split(' ')
-                            .joinToString(" ") {
-                                it.replaceFirstChar(Char::uppercaseChar)
-                            } to it.second
-                    }
                     .take(5)
 
                 val maxValue = counts.maxOfOrNull { it.second } ?: 0
 
-                val topGenresUI = counts.map {
+                val topGenresUI = counts.map { (genre, count) ->
                     TopGenreUI(
-                        genreName = it.first,
-                        genreChartValue = it.second.toFloat() / maxValue.toFloat()
+                        genreName = genre.capitalizeWords(),
+                        genreChartValue = count.toFloat() / maxValue.toFloat()
                     )
                 }.sortedByDescending { it.genreChartValue }
 
@@ -50,4 +45,5 @@ class GetTopGenresFromArtists(
             .catch { emit(emptyList()) }
             .flowOn(dispatchers.background)
     }
+
 }
