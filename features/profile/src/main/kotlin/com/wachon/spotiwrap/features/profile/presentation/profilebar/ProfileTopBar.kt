43b2580d3dd079ab878fc7617989d6413a3406cb
Nacho Ramos Sanchez <nachoramossanchez@gmail.com>
2023-05-21 12:58:12 +0200

    Update ProfileTopBar.kt

--- features/profile/src/main/kotlin/com/wachon/spotiwrap/features/profile/presentation/profilebar/ProfileTopBar.kt
+++ features/profile/src/main/kotlin/com/wachon/spotiwrap/features/profile/presentation/profilebar/ProfileTopBar.kt
@@ -3,6 +3,7 @@ package com.wachon.spotiwrap.features.profile.presentation.profilebar
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,8 +37,8 @@ import com.wachon.spotiwrap.core.design.theme.SmallTitle
 import com.wachon.spotiwrap.core.design.theme.SpotiWrapTheme
 import com.wachon.spotiwrap.core.design.theme.SubBody
 import com.wachon.spotiwrap.core.design.theme.Title
-import com.wachon.spotiwrap.features.profile.R
 import com.wachon.spotiwrap.features.profile.presentation.model.CurrentTrackUI
+import com.theapache64.rebugger.Rebugger
 import com.wachon.spotiwrap.features.profile.presentation.model.UserUI
 
 @Composable
@@ -44,55 +46,74 @@ fun ProfileTopBar(
     user: UserUI?,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .statusBarsPadding()
-            .padding(
-                vertical = 16.dp
-            )
-            .padding(horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
 
-        ProfileGreetings { user }
-        ProfileImage { user?.image ?: "" }
-    }
-}
-
-@Composable
-fun ProfileGreetings(
-    modifier: Modifier = Modifier,
-    user: () -> UserUI?,
-) {
+    Rebugger(
+        trackMap = mapOf(
+            "user" to user,
+            "modifier" to modifier,
+        ),
+    )
 
     Column(
         modifier = modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(top = 16.dp)
+            .padding(horizontal = 16.dp),
     ) {
-        TextNoPadding(
-            text = user()?.displayName ?: "",
-            style = SmallTitle,
-            color = MaterialTheme.colorScheme.onBackground,
-        )
 
-        if (user()?.currentTrackUI != null) {
-            ProfileCurrentSong(currentTrackUI = user()?.currentTrackUI)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            ProfileUserName(
+                modifier = Modifier.weight(1F),
+                userDisplayName = user?.displayName ?: ""
+            )
+
+            ProfileUserImage(
+                imageUrl = user?.image ?: ""
+            )
+        }
+
+        if (user?.currentTrackUI != null) {
+            ProfileCurrentSong(
+                currentTrackUI = user.currentTrackUI
+            )
         }
     }
 }
 
 @Composable
-fun ProfileImage(
+fun ProfileUserName(
     modifier: Modifier = Modifier,
-    imageUrl: () -> String
+    userDisplayName: String
+) {
+
+    TextNoPadding(
+        modifier = modifier,
+        text = userDisplayName,
+        style = SmallTitle,
+        color = MaterialTheme.colorScheme.onBackground,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+
+}
+
+@Composable
+fun ProfileUserImage(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
 ) {
 
     AsyncImage(
-        model = imageUrl(),
+        model = imageUrl,
         contentScale = ContentScale.Crop,
         contentDescription = null,
         modifier = modifier
+            .padding(start = 8.dp)
             .size(65.dp)
             .border(
                 width = 2.dp,
@@ -102,69 +123,113 @@ fun ProfileImage(
             .padding(6.dp)
             .clip(CircleShape),
     )
+
 }
 
 @Composable
 fun ProfileCurrentSong(
     modifier: Modifier = Modifier,
-    currentTrackUI: CurrentTrackUI?
+    currentTrackUI: CurrentTrackUI
 ) {
 
+    Rebugger(
+        trackMap = mapOf(
+            "modifier" to modifier,
+            "currentTrackUI" to currentTrackUI,
+        ),
+    )
+
+    Spacer(modifier = Modifier.height(8.dp))
     AnimatedVisibility(
+        modifier = modifier
+            .clip(RoundedCornerShape(10))
+            .background(MaterialTheme.colorScheme.surface),
         visible = true,
         enter = fadeIn(),
         exit = fadeOut()
     ) {
 
-        Spacer(modifier = Modifier.height(8.dp))
         Column(
             modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 16.dp, top = 8.dp)
         ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(4.dp),
-                verticalAlignment = Alignment.Top
-            ) {
-                TextNoPadding(
-                    text = "Currenty playing",
-                    style = Title
-                )
-                LottieImage(
-                    modifier = modifier
-                        .size(35.dp),
-                    animation = DesignR.raw.equalizer,
-                    tintColor = MaterialTheme.colorScheme.primary
-                )
-            }
+            CurrentTrackTitle()
             Spacer(modifier = Modifier.height(4.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                AsyncImage(
-                    model = currentTrackUI!!.image,
-                    contentScale = ContentScale.Crop,
-                    contentDescription = null,
-                    modifier = modifier
-                        .size(30.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Column {
-                    TextNoPadding(
-                        text = currentTrackUI.title,
-                        style = Body.copy(
-                            fontSize = 12.sp,
-                            fontWeight = FontWeight.W600
-                        ),
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                    TextNoPadding(
-                        text = currentTrackUI.artist,
-                        style = SubBody.copy(fontSize = 10.sp),
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.8f)
-                    )
-                }
-            }
+            CurrentTrackInfo(
+                trackImage = currentTrackUI.image,
+                trackTitle = currentTrackUI.title,
+                trackArtist = currentTrackUI.artist
+            )
+        }
+    }
+
+}
+
+@Composable
+fun CurrentTrackTitle(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextNoPadding(
+            text = "Currenty playing",
+            style = Title.copy(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.W600
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        LottieImage(
+            modifier = Modifier
+                .size(35.dp),
+            animation = DesignR.raw.equalizer,
+            tintColor = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Composable
+fun CurrentTrackInfo(
+    modifier: Modifier = Modifier,
+    trackImage: String,
+    trackTitle: String,
+    trackArtist: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = trackImage,
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier
+                .size(30.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            TextNoPadding(
+                text = trackTitle,
+                style = Body.copy(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.W600
+                ),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            TextNoPadding(
+                text = trackArtist,
+                style = SubBody.copy(fontSize = 10.sp),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.8f)
+            )
         }
     }
 }
