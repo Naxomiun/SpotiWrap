b8f82f42b29ad412ca385ddab2f8ca17e3644ae7
Balta <baltax.75@gmail.com>
2023-12-10 20:58:12 +0100

    Fixes worker error

--- core/data/src/main/kotlin/com/wachon/spotiwrap/data/worker/SyncWorker.kt
+++ core/data/src/main/kotlin/com/wachon/spotiwrap/data/worker/SyncWorker.kt
@@ -1,15 +1,23 @@
 package com.wachon.spotiwrap.data.worker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingWorkPolicy
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import com.wachon.spotiwrap.core.common.R.drawable.fame_new
 import com.wachon.spotiwrap.core.common.dispatchers.DispatcherProvider
+import com.wachon.spotiwrap.core.data.R
 import com.wachon.spotiwrap.data.repository.ArtistsRepository
 import com.wachon.spotiwrap.data.repository.TracksRepository
 import com.wachon.spotiwrap.data.repository.UserRepository
@@ -43,6 +51,37 @@ class SyncWorker(
             Result.retry()
         }
 
+    }
+
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "NOTIFICATION_CHANNEL_ID",
+                "NOTIFICATION_CHANNEL_NAME",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, "NOTIFICATION_CHANNEL_ID")
+            .setSmallIcon(fame_new)
+            .setOngoing(true)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setLocalOnly(true)
+            .setVisibility(NotificationCompat.VISIBILITY_SECRET)
+            .setContent(
+                RemoteViews(
+                    applicationContext.packageName,
+                    R.layout.sync_notification_layout
+                )
+            )
+            .build()
+        return ForegroundInfo(1337, notification)
     }
 
     companion object {
